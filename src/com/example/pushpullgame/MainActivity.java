31b8b0f8ee002e0a2e65fbef21ce8debd0d774cd
unknown <10005009@wyhqo54000099.whq.wistron>
2013-05-16 00:28:08 +0800

    salty add animation

--- src/com/example/pushpullgame/MainActivity.java
+++ src/com/example/pushpullgame/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.pushpullgame;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.DrawableContainer;
+import android.view.InputEvent;
 
 public class MainActivity extends Activity {
 
+	AnimationDrawable frameAnimation;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView)findViewById(R.id.imageView1);
+        img.setBackgroundResource(R.drawable.approach_animation);
+        frameAnimation = (AnimationDrawable) img.getBackground();
         
     }
 
@@ -21,4 +32,17 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void onWindowFocusChanged(boolean hasFocus) {
+    	super.onWindowFocusChanged(hasFocus);
+    	if (hasFocus)
+    		frameAnimation.start();
+    }
+    
+//    public boolean onTouchEvent(MotionEvent event) {
+//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//        	frameAnimation.start();
+//        	return true;
+//        }
+//        return super.onTouchEvent(event);
+//    }
 }
